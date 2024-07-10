80b7b4661331be55eac571241cb4b8a9b427b8b2
Sangin Han <hugedreamhd@gmail.com>
2024-07-11 01:02:48 +0900

    feat: flutter 01 과제 1

---
+++ project/learn_flutter_togerther/lib/01_widget/creatorprofile/presentation/main_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+
+class CreatorProfile extends StatelessWidget {
+  const CreatorProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          child: Row(
+            children: [
+
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
